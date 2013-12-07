9fe73f63487a29e730e70e766999dcb3ec7dea9f
Smit Shah <who828@gmail.com>
2013-12-07 11:49:20 +0530

    Use synchronize chdir for unix_workers and remove synchronize for pwd

--- lib/bundler/shared_helpers.rb
+++ lib/bundler/shared_helpers.rb
@@ -1,5 +1,6 @@
 require 'pathname'
 require 'rubygems'
+require 'monitor'
 
 require 'bundler/constants'
 require 'bundler/rubygems_integration'
@@ -33,28 +34,15 @@ module Bundler
       find_gemfile
     end
 
-    if Bundler.current_ruby.mswin? || Bundler.current_ruby.jruby?
-      require 'monitor'
-      @chdir_monitor = Monitor.new
-      def chdir(dir, &blk)
-        @chdir_monitor.synchronize do
-          Dir.chdir dir, &blk
-        end
-      end
-
-      def pwd
-        @chdir_monitor.synchronize do
-          Dir.pwd
-        end
-      end
-    else
-      def chdir(dir, &blk)
+    @chdir_monitor = Monitor.new
+    def chdir(dir, &blk)
+      @chdir_monitor.synchronize do
         Dir.chdir dir, &blk
       end
+    end
 
-      def pwd
-        Dir.pwd
-      end
+    def pwd
+      Dir.pwd
     end
 
     def with_clean_git_env(&block)
